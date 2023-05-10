d0c51b945c78a8e168f47a7529ec50e299ab97cd
Sami EL KATEB <sami.elkateb@gmail.com>
2023-05-10 22:19:39 +0200

    refactor: dock configuration

---
+++ scripts/dock.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+APP_PATHS=()
+for arg in "${@}"; do
+   if test -d "/System/Applications/$arg.app"; then
+      APP_PATHS+=("/System/Applications/$arg.app")
+   elif test -d "/Applications/$arg.app"; then 
+      APP_PATHS+=("/Applications/$arg.app")
+   else 
+     echo "$arg does not exist in /Applications or in /System/Applications"
+     exit 1
+   fi
+done
+
+CURRENT_DOCK=($(defaults read com.apple.dock persistent-apps|grep "\"_CFURLString\""|awk -F '"' '{print $4}'))
+i=0
+DIFFERENT=0
+for DOCK_APP in "${CURRENT_DOCK[@]}"; do
+   echo "$DOCK_APP"|grep -s "file://${APP_PATHS["$i"]}\b" &>/dev/null
+   if [ "$?" -ne 0 ];then
+    DIFFERENT=1
+   fi
+   i=$(($i + 1))
+done
+
+if [ "$DIFFERENT" = 0 ];then
+  echo "Dock not modified."
+  exit 0
+fi
+
+defaults write com.apple.dock persistent-apps -array
+for path in "${APP_PATHS[@]}"; do
+  defaults write com.apple.dock persistent-apps -array-add "<dict>
+    <key>tile-data</key>
+    <dict>
+      <key>file-data</key>
+      <dict>
+        <key>_CFURLString</key>
+        <string>$path</string>
+        <key>_CFURLStringType</key>
+        <integer>0</integer>
+      </dict>
+    </dict>
+  </dict>"
+done
+
+echo "Dock updated."
+killall Dock
